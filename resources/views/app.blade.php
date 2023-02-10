08018f6961b55db779f585ec3bc99d699b1fe377
Dihak <dihak@digitalkode.com>
2023-02-10 10:24:53 +0700

    chore: add google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,16 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YLH87ZDD3R"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YLH87ZDD3R');
+        </script>
     </head>
     <body class="font-sans antialiased min-h-full flex flex-col">
         @inertia
