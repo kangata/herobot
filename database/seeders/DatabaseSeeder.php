829833926981c62811fc8c07b89394ba6d702b49
Dihak <dihak@digitalkode.com>
2024-08-03 20:18:22 +0700

    Updated the user seeder to create a user with a personal team.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::factory()->withPersonalTeam()->create([
             'name' => 'Test User',
             'email' => 'user@example.com',
         ]);
