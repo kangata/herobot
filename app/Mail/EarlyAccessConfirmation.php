15851eb5ab42dd7691b17410f3892f8994d0c469
Ata <quetzalskeith@gmail.com>
2025-06-03 20:27:36 +0700

    Fix send email not running on background

--- app/Mail/EarlyAccessConfirmation.php
+++ app/Mail/EarlyAccessConfirmation.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\EarlyAccess;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class EarlyAccessConfirmation extends Mailable
+class EarlyAccessConfirmation extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
