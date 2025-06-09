7bbd5b943f19fba903fc3a24af8f625af766f182
Dihak <dihak@digitalkode.com>
2025-06-09 20:43:43 +0700

    fix: https://github.com/herobot-id/herobot/issues/17

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,57 +1,37 @@
 {pkgs, ...}: {
-  # Use stable channel
   channel = "stable-24.11";
 
-  # Required packages based on README setup
   packages = [
     pkgs.git
     pkgs.nodejs
     pkgs.docker
   ];
 
-  # Enable required services
   services = {
     # Docker for containerization (required for Sail)
     docker.enable = true;
   };
 
-  # Workspace startup commands from README using Sail
   idx.workspace = {
     onStart = {
-      # Setup environment file if not exists
       env-setup = ''
         if [ ! -f .env ]; then
           cp .env.example .env
-          # Generate application key
-          ./vendor/bin/sail artisan key:generate --ansi
         fi
+
+        sed -i "s/^APP_URL=.*/APP_URL=https:\/\/$WEB_HOST/" .env
+
+        echo "APP_PORT=8888" >> .env
+        
+        echo "SUPERVISOR_PHP_USER=root" >> .env
       '';
 
-      # Initial Sail setup with Composer
-      sail-setup = ''
-        docker run --rm \
-          -u "$(id -u):$(id -g)" \
-          -v "$(pwd):/var/www/html" \
-          -w /var/www/html \
-          laravelsail/php84-composer:latest \
-          composer install --ignore-platform-reqs
-      '';
-      
-      # Start Sail services in detached mode
-      sail-up = "./vendor/bin/sail up -d";
+      docker-up = "docker compose up";
 
-      # Install NPM dependencies through Sail
-      sail-npm-install = "./vendor/bin/sail npm install";
-      
-      # Start development servers through Sail
-      sail-npm-dev = "./vendor/bin/sail npm run dev";
-      sail-reverb = "./vendor/bin/sail artisan reverb:start";
-      sail-whatsapp = "./vendor/bin/sail artisan whatsapp:start";
+      # Default files to open when workspace starts
+      default.openFiles = [
+        "README.md"
+      ];
     };
-
-    # Default files to open
-    default.openFiles = [
-      "README.md"
-    ];
   };
-} 
+}
